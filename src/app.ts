7b1b2e65f6f2a778de79d9d3e409796ea98fab27
Himinnn <li.cs112062207@gapp.nthu.edu.tw>
2026-05-20 20:35:00 +0800

    test: verify test not pass case

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ export function buildApp(options: FastifyServerOptions = {}) {
     ...options
   });
 
-  
   app.get('/', async () => {
     return {
       message: 'CI/CD Lab Fastify app is running',
@@ -16,7 +15,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
 
   app.get('/health', async () => {
     return {
-      status: 'ok'
+      status: 'okk'
     };
   });
 
