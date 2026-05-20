c4108ce056c4c2218e561fafaadfc632fc2f5575
Himinnn <li.cs112062207@gapp.nthu.edu.tw>
2026-05-20 17:25:56 +0800

    fix format with npm run format

--- test/app.test.ts
+++ test/app.test.ts
@@ -9,7 +9,6 @@ describe('Fastify app', () => {
       url: '/health'
     });
 
-    
     expect(response.statusCode).toBe(200);
     expect(response.json()).toEqual({ status: 'ok' });
     await app.close();
