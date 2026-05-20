d94603406d44b50c2c3aa24397e64e579ed30cee
Himinnn <li.cs112062207@gapp.nthu.edu.tw>
2026-05-20 20:33:19 +0800

    test: verify ci check on format error

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
     ...options
   });
 
+  
   app.get('/', async () => {
     return {
       message: 'CI/CD Lab Fastify app is running',
